813a462ad78ebe53d0a12282b06bc380ee9b36c8
Daniel Jorge <djpgonzaga@gmail.com>
2019-05-19 16:37:37 -0300

    basic class

---
+++ TIGrafos/TIGrafos/Grafo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIGrafos
+{
+    class Grafo
+    {
+        public List<Vertice> listaVertices;
+
+        internal List<Vertice> ListaVertices
+        {
+            get => listaVertices;
+            set => listaVertices = value;
+        }
+    }
+}
